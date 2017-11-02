b2dee9ae02d413d9543c3a15679660a3e01c26e8
Dave Walker <dave@happybits.co>
2017-11-02 11:19:08 -0700

    fix swift compile errors

--- ImagePickerSheetController/ImagePickerSheetController/Sheet/SheetController.swift
+++ ImagePickerSheetController/ImagePickerSheetController/Sheet/SheetController.swift
@@ -156,7 +156,8 @@ class SheetController: NSObject {
         actionHandlingCallback?()
         action.handle(numberOfSelectedAssets)
     }
-    
+
+    @objc
     func handleCancelAction() {
         let cancelAction = actions.filter { $0.style == .cancel }
                                   .first
